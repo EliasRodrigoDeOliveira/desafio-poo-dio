1f668756764f01ccca45a2f7ededd84e3d3edacd
EliasRodrigoDeOliveira <eliasoliveirarodrigo@gmail.comgit config --global user.name EliasRodrigoDeOliveiragit config --global user.email eliasoliveirarodrigo@gmail.com>
2022-03-13 12:35:07 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -20,8 +19,42 @@ public class Main {
         mentoria.setDescricao("descricao mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devElias = new Dev();
+        devElias.setNome("Elias");
+        devElias.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdo Inscritos Elias:" + devElias.getConteudosInscritos());
+        devElias.progredir();
+        devElias.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdo Inscritos Elias:" + devElias.getConteudosInscritos());
+        System.out.println("Conteúdo Concluidos Elias:" + devElias.getConteudosConcluidos());
+        System.out.println("XP:" + devElias.calcularTotalXp());
+
+        System.out.println("-------");
+
+        Dev devRaquel = new Dev();
+        devRaquel.setNome("Raquel");
+        devRaquel.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdo Inscritos Raquel:" + devRaquel.getConteudosInscritos());
+        devRaquel.progredir();
+        devRaquel.progredir();
+        devRaquel.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdo Inscritos Raquel:" + devRaquel.getConteudosInscritos());
+        System.out.println("Conteúdo Concluidos Raquel:" + devRaquel.getConteudosConcluidos());
+        System.out.println("XP:" + devRaquel.calcularTotalXp());
+
+
     }
 }
